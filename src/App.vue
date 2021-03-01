c183fc10fd495170dcbe4bff78bf26676d6b3317
Andrea Stagi <stagi.andrea@gmail.com>
2021-03-01 22:33:10 +0100

    First import

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <o-field :variant="fieldType" :message="fieldMessage" label="Search your Pokèmon">
+      <o-input type="text" value="" v-model="search"> </o-input>
+    </o-field>
+    <div class="buttons-container">
+      <o-button @click="searchPokemon" :disabled="search.length === 0">Search Pokèmon</o-button>
+    </div>
+    <img :src="pokemon ? pokemon.sprites.front_default : null">
+    <o-table :data="data" :columns="columns"></o-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      search: '',
+      pokemon: null,
+      isPokemonValid: null,
+      data: [],
+      columns: [
+        {
+          field: 'stat.name',
+          label: 'Stat',
+          numeric: true
+        },
+        {
+          field: 'base_stat',
+          label: 'Value'
+        },
+        {
+          field: 'effort',
+          label: 'Effort'
+        }
+      ]
+    }
+  },
+  components: {},
+  computed: {
+    fieldType(){
+      if (this.isPokemonValid == true) {
+        return 'success'
+      } else if (this.isPokemonValid == false) {
+        return 'error'
+      }
+      return null
+    },
+    fieldMessage(){
+      if (this.isPokemonValid == true) {
+        return 'Catch it!'
+      } else if (this.isPokemonValid == false) {
+        return 'Pokèmon not found!'
+      }
+      return null
+    }
+  },
+  methods: {
+    searchPokemon () {
+      this.pokemon = null
+      fetch(`https://pokeapi.co/api/v2/pokemon/${this.search}`)
+      .then(response => response.json())
+      .then(data => {
+        this.pokemon = data
+        this.data = this.pokemon.stats
+        this.isPokemonValid = true
+      })
+      .catch(() => {
+        this.isPokemonValid = false
+      });
+    }
   }
 }
 </script>
 
 <style>
+@font-face {
+  font-family: "Press Start 2P";
+  src: local("Press Start 2P"),
+   url(./fonts/PressStart2P-Regular.ttf) format("truetype");
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 3rem;
+}
+.buttons-container {
+  padding: 1rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Button, Config, Field, Input, Table, Pagination} from '@oruga-ui/oruga';
+
+import "nes.css/css/nes.min.css";
+import '@oruga-ui/oruga/dist/oruga.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Field)
+Vue.use(Input)
+Vue.use(Table)
+Vue.use(Pagination)
+Vue.use(Config, {
+  button: {
+    override: true,
+    rootClass: 'nes-btn',
+    disabledClass: 'is-disabled'
+  },
+  field: {
+    rootClass: "nes-field",
+    messageClass: "nes-text",
+    variantClass: (suffix) => {
+      return `is-${suffix}`
+    }
+  },
+  input: {
+    inputClass: "nes-input",
+    variantClass: (suffix) => {
+      return `is-${suffix}`
+    }
+  },
+  pagination: {
+    linkClass: 'nes-btn'
+  },
+  table: {
+    tableClass: "nes-table is-bordered"
+  }
+})
 
 new Vue({
   render: h => h(App),
